688fd07374b9090443711b12c750cb86f1f0f089
Tushar <tusharrgajwani@gmail.com>
2022-07-09 21:41:39 +0530

    Initial Commit

---
+++ Hack News/Hack_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hack_NewsApp.swift
+//  Hack News
+//
+//  Created by Admin on 09/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Hack_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
